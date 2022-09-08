a2eff077702da721d3ca931b4635e6b90bdfcbb2
Popi Mudau <popimudau95@gmail.com>
2022-09-08 14:47:05 -0700

    Print Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * main - Prints given text
@@ -7,6 +9,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	char quo[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+	write(2, quo' 59);
 	return (1);
 }
