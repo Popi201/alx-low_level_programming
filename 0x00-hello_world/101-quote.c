19193c746abb044316f43920267c69c0f0fff698
Popi Mudau <popimudau95@gmail.com>
2022-09-08 15:04:10 -0700

    Print Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 
 /**
- * main - Prints given text
- *
- * Return:1
+ *    main - prints exactly
+ *     
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
-	write(2, quo' 59);
-	return (1);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		return (1);
 }
