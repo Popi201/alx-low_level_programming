21c86ca9bd89babb4ad0a892ab91d4609e2852ad
Popi Mudau <popimudau95@gmail.com>
2022-09-09 05:47:56 -0700

    Print Tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,12 +5,12 @@
  * Description: Print the alphabet in reverse order in lowercase.
  *
  * Return: 0
- *     */
+ */
 int main(void)
 {
 		char c;
-
-	       	for (c = 'z'; c >= 'a'; c--)
+		
+		for (c = 'z'; c >= 'a'; c--)
 		{
 			putchar(c);
 		}
