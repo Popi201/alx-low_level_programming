9f28ba00879ffe6286f106554cee7c26efd06cde
Popi Mudau <popimudau95@gmail.com>
2022-09-09 04:29:27 -0700

    Print Alphabet 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+		char x;
+
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			putchar(x);
+		}	
+		putchar('\n');
+		return (0);
+
+}
