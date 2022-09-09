a1fd54768cc4bc5ee7e824925c51f9f0a742c39d
Popi Mudau <popimudau95@gmail.com>
2022-09-09 06:52:23 -0700

    Print Combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ *  *  main - main block
+ *   *  Description: Print all possible combinations of two digits.
+ *    *  Numbers must be separated by commas and a space.
+ *     *   01 and 10 are considered as the same combination of the two digits.
+ *      * Print only the smallest combination of two digits.
+ *       * Numbers should be printed in ascending order, with two digits.
+ *        * You can only use `putchar`.
+ *         * You can only use `putchar` up to 5 times.
+ *          * You are not allowed to use any variable of type 'char'.
+ *           * Return: 0
+ *            */
+int main(void)
+{
+		int i, e;
+
+			i = 48;
+				e = 48;
+
+					while (e < 58)
+							{
+										i = 48;
+												while (i < 58)
+															{
+																			if (e != i && e < i)
+																							{
+																												putchar(e);
+																																putchar(i);
+																																				if (i == 57 && e == 56)
+																																									{
+																																															break;
+																																																			}
+																																								putchar(',');
+																																												putchar(' ');
+																																															}
+																						i++;
+																								}
+														e++;
+															}
+						putchar('\n');
+							return (0);
+}
