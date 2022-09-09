8c949ce43233ab40de8333842976f903bd626d44
Popi Mudau <popimudau95@gmail.com>
2022-09-09 07:01:32 -0700

    Print comb 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,38 +1,11 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@Popi201 
-candystarr1991
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x01-variables_if_else_while/102-print_comb5.c
-@candystarr1991
-candystarr1991 102-print_comb5.c
-Latest commit 61ad945 on 5 Jun
- History
-  1 contributor
-  48 lines (46 sloc)  620 Bytes
-
 #include <stdio.h>
 
-  /**
-   *  * main - Prints numbers between 00 to 99.
-   *   *
-   *    *  Return: Always 0 (Success)
-   *     */
-  int main(void)
+/**
+ *  * main - Prints numbers between 00 to 99.
+ *   *
+ *    *  Return: Always 0 (Success)
+ *     */
+int main(void)
 {
 	int i, e, g, h, op1, op2;
 
